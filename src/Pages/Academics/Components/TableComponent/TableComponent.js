62a924ad0dfba2a7814bad073df52a1e362d62ce
Yashvardhan Mathur <yvmatts@Yashvardhans-MacBook-Pro.local>
2021-05-14 23:03:38 +0530

    Academics skeleton page created, Folder structure altered

--- src/Pages/Academics/Components/TableComponent/TableComponent.js
+++ src/Pages/Academics/Components/TableComponent/TableComponent.js
@@ -1,3 +1,4 @@
+import './styles/TableComponent.css'
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
@@ -5,13 +6,12 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
-import {courseList} from '../../Pages/Academics/courses'
 
 const TableComponent = (props) => {
     return (  
-        <div className="table">
+        <div className="table-container">
             <TableContainer component={Paper}>
-                <Table aria-label="customized table">
+                <Table aria-label="customized table" className="table">
                     <TableHead>
                         <TableRow>
                             {
@@ -23,18 +23,19 @@ const TableComponent = (props) => {
                     </TableHead>
                     <TableBody>
                         {
-                            courseList.map((course) => (
-                                <TableRow key={course.name}>
+                            Object.keys(props.courseInfo).map((keyName, keyIndex) => (
+                                <TableRow key={keyIndex} className="table-cell">
                                     <TableCell component="th" scope="row">
-                                    {course.name}
+                                    {keyName}
                                     </TableCell>
-                                    <TableCell align="right">{course.avg}</TableCell>
+                                    <TableCell align="right">{props.courseInfo[keyName]}</TableCell>
                                 </TableRow>
                             ))
                         }
                     </TableBody>
                 </Table>
-            </TableContainer>   
+            </TableContainer>  
+            
         </div>
       );
 }
