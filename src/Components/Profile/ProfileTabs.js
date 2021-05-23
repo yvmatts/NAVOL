30a5a6b7bd2dc50564e19cef3b90cd31695e287f
Yashvardhan Mathur <yvmatts@gmail.com>
2021-05-23 15:52:00 +0530

    Added AccountsTab in profile for Password Change

--- src/Components/Profile/ProfileTabs.js
+++ src/Components/Profile/ProfileTabs.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { Tabs, Tab, Button } from 'react-bootstrap/';
 import { MdEdit, MdCheck } from "react-icons/md";
+import 'Styles/Profile/Components/ProfileTabs.css';
 import BasicTab from './BasicTab';
 import MedicalTab from './MedicalTab';
-import 'Styles/Profile/Components/ProfileTabs.css';
 import DgcaTab from './DgcaTab';
+import AccountsTab from './AccountsTab';
 
 const ProfileTabs = () => {
     
@@ -12,6 +13,7 @@ const ProfileTabs = () => {
     const [disableBasic, setDisableBasic] = useState(false);
     const [disableMedical, setDisableMedical] = useState(false);
     const [disableDgca, setDisableDgca] = useState(false);
+    const [disableAccounts, setDisableAccounts] = useState(false);
     const [tabId, setTabId] = useState("basic");
     const [btn, setBtn] = useState(<Button variant="outline-danger" id={tabId} className="edit-btn">Edit &nbsp;<MdEdit/></Button>);
 
@@ -41,16 +43,25 @@ const ProfileTabs = () => {
                     setDisableBasic(false);
                     setDisableMedical(true);
                     setDisableDgca(true);
+                    setDisableAccounts(true);
                     break;
                 case "medical":
                     setDisableBasic(true);
                     setDisableMedical(false);
                     setDisableDgca(true);
+                    setDisableAccounts(true);
                     break;
                 case "dgca":
                     setDisableBasic(true);
                     setDisableMedical(true);
                     setDisableDgca(false);
+                    setDisableAccounts(true);
+                    break;
+                case "accounts":
+                    setDisableBasic(true);
+                    setDisableMedical(true);
+                    setDisableDgca(true);
+                    setDisableAccounts(false);
                     break;
                 default:
                     break;
@@ -60,6 +71,7 @@ const ProfileTabs = () => {
             setDisableBasic(false);
             setDisableMedical(false);
             setDisableDgca(false);
+            setDisableAccounts(false);
         }
     }
 
@@ -107,6 +119,16 @@ const ProfileTabs = () => {
                     </div>
                     <div className="tab-bottom">
                         <DgcaTab disableForm={disableForm}/>
+                    </div>
+                </Tab>
+                <Tab eventKey="accounts" title="Accounts" disabled={disableAccounts}>
+                    <div className="d-flex btn">
+                        <div className="ml-auto" onClick={handleEdit}>
+                            {btn}
+                        </div>
+                    </div>
+                    <div className="tab-bottom">
+                        <AccountsTab disableForm={disableForm} />
                     </div>
                 </Tab>
             </Tabs>
